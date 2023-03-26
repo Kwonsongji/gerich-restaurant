bd9804f6ce386836eb62646a88e6160ac616c072
KwonSongji <mariecamillecalone@gmail.com>
2023-03-26 22:49:22 +0200

    special menu

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-import './MenuItem.css';
+import './index.css';
 
 const MenuItem = () => (
-  <div>
-    MenuItem
+  <div className="app__menuitem">
+    <div className="app__menuitem-head">
+      <div className="app__menuitem-name">
+        <p className="p__cormorant" style={{color: '#DCCA87'}}></p>
+      </div>
+    </div>
   </div>
 );
 
