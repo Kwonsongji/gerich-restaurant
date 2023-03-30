e978e397fe3bcfaac0ba187f4a0fb73cfbad10b1
KwonSongji <mariecamillecalone@gmail.com>
2023-03-30 23:22:34 +0200

    Gallery component

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram"/>
+      </div>
+    </div>
+    
+  )
+};
 
 export default Gallery;
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Laurels.css';
 
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="award_img" />
+    <div className="app__laurels_award-card_content">
+      <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+      <p className="p__opensans">{subtitle}</p>
+        </div>
+  </div>
+)
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="laurel">
+    <div className="app__logo-img">
+      <img src={images.logo} alt="logo_img" />
+    </div>
+    <div className="app__wrapper-info">
+      <SubHeading title="Award & recognition" />
+      <h1 className="headtext__cormorant"> Our Laurels </h1>
+      <div className="app__laurels_awards">
+        {data.awards.map((award) =>
+          <AwardCard award={award} key={award.title} />
+        )}
+        
+      </div>
+    </div>
+    <div className="app__wrapper-img">
+      {<img src={images.laurels} alt="laurels_img" />}
+    </div>
+
   </div>
 );
 
