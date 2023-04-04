5766ae0f52a114bdcd6176627a86b1b65acad5b6
KwonSongji <mariecamillecalone@gmail.com>
2023-04-04 20:03:55 +0200

    Gallery component

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -29,15 +29,15 @@ const Gallery = () => {
       <div className="app__gallery-images">
         <div className="app__gallery-images_container" ref={scrollRef}>
           {galleryImages.map((image, index) => (
-            <div className="app__gallery-images_cards flex__center" key={`gallery_images-${index + 1}`}>
+            <div className="app__gallery-images_card flex__center" key={`gallery_images-${index + 1}`}>
               <img src={image} alt="gallery__image" />
               <BsInstagram className="gallery__image-icon"/>
             </div>
             ))}
         </div>
-        <div className="app__gallery-images_arrow">
-          <BsArrowLeftShort className="gallery__arrow-icons" onClick={() => scroll('left')} />
-          <BsArrowRightShort className="gallery__arrow-icons" onClick={() => scroll('right')} />
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
         </div>
       </div>
     </div>
